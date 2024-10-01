0a02b95716aa370bc50e0c5a24fdd08da14f8bc3
ArtistsEdge <lucycorwinart@gmail.com>
2024-10-01 17:15:41 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InstagramReelTemplate from './InstagramReelTemplate';
+
+function App() {
+  return (
+    <div className="App">
+      <InstagramReelTemplate />
+    </div>
+  );
+}
+
+export default App;
